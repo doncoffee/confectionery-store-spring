ed29960ad10dc170b53904db29bb7d2ce058d8b9
Ilya <iliaabr5@gmail.com>
2023-04-06 18:27:28 +0300

    Initial commit

---
+++ src/main/java/by/academy/confectionerystorespring/ConfectioneryStoreSpringApplication.java
@@ -0,0 +1,13 @@
+package by.academy.confectionerystorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfectioneryStoreSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfectioneryStoreSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/academy/confectionerystorespring/ConfectioneryStoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package by.academy.confectionerystorespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfectioneryStoreSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
